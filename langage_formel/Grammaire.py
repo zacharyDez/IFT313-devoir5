b7ff30dff4b3152855b9f6f081805d4a122c09ea
dezz2201 <zachary.deziel@usherbrooke.ca>
2020-08-06 10:32:30 -0400

    implemented basic structure for adding and removing rules to grammar

---
+++ langage_formel/Grammaire.py
@@ -0,0 +1,26 @@
+class Grammaire:
+
+    def __init__(self) -> None:
+        # rules is a dict where key is symbole
+        #   and values is list of outputs associated with symbole
+        self.rules = dict()
+
+    def add_rule(self, symbole: str, output: str) -> None:
+        self.rules[symbole] = output
+
+    def remove_rule(self, symbole: str, output: str) -> None:
+        if symbole not in self.rules:
+            raise AttributeError("Symbole is not in rules")
+
+        if len(self.rules[symbole]) == 1:
+            self.rules.pop(symbole)
+        else:
+            sym_rules = self.rules.get(symbole)
+            self.rules[symbole] = [rule for rule in sym_rules if rule != output]
+
+    def has_rule(self, symbole: str, output: str) -> bool:
+        # key in rules checked before second cond
+        if symbole in self.rules and output in self.rules[symbole]:
+            return True
+
+        return False
---
+++ langage_formel/tests/test_G.py
@@ -0,0 +1,23 @@
+import pytest
+from langage_formel.Grammaire import Grammaire
+
+
+def test_add_rule_to_G():
+    G = Grammaire()
+    G.add_rule("S", "a")
+
+    assert G.has_rule("S", "a")
+
+
+def test_remove_nonexistant_rule_from_G():
+    G = Grammaire()
+    with pytest.raises(AttributeError) as e_info:
+        G.remove_rule("S", "a")
+
+
+def test_remove_rule_from_G():
+    G = Grammaire()
+    G.add_rule("S", "a")
+    G.remove_rule("S", "a")
+
+    assert not G.has_rule("S", "a")
